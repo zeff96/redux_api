7963ae5f0e285f91b20f434fe1f098594e4aa0a0
zeff96 <zeffo1975@gmail.com>
2023-04-06 05:24:52 +0300

    Add FetchedUsers component rendering list of firstname and lastname of users.

--- src/components/FetchedUsers.js
+++ src/components/FetchedUsers.js
@@ -1,8 +1,25 @@
 import { useSelector } from "react-redux";
 
 const FetchedUsers = () => {
-  const { users, isLoading } = useSelector((state) => state.users);
-  return <div>FetchedUsers</div>;
+  const { users, isLoading, error } = useSelector((state) => state.users);
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  } else if (error) {
+    return <h3>{error}</h3>;
+  }
+
+  const fetchedUsers = users.map((user) => (
+    <li key={users.id}>
+      <p>{user.firstName}</p>
+      <p>{user.lastName}</p>
+    </li>
+  ));
+
+  return (
+    <ul>
+      <fetchedUsers />
+    </ul>
+  );
 };
 
 export default FetchedUsers;
